1347eb36fdbc406f4fb2848ca0e9d81bb02f7044
chas s <chasdabigone@gmail.com>
2023-10-30 13:10:03 -0500

    Update __init__.py

--- ascii_magic/__init__.py
+++ ascii_magic/__init__.py
@@ -7,6 +7,6 @@ try:
 except:
   pass
 
-from ascii_magic._ascii_magic import AsciiArt, Front, Back  # noqa
-from ascii_magic.constants import PALETTE, CHARS_BY_DENSITY  # noqa
-from ascii_magic.functions import *  # noqa
+from ascii_magic.ascii_magic._ascii_magic import AsciiArt, Front, Back  # noqa
+from ascii_magic.ascii_magic.constants import PALETTE, CHARS_BY_DENSITY  # noqa
+from ascii_magic.ascii_magic.functions import *  # noqa
